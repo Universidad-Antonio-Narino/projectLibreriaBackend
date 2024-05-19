140234ac708b6339b52382d07c193c8413471635
Santiago Angel Mancera <eyerforeversam@gmail.com>
2024-05-18 21:33:35 -0500

    new things

--- backend/libros/models.py
+++ backend/libros/models.py
@@ -1,9 +1,28 @@
 from django.db import models
 
-# Create your models here.
-class Libros(models.Model):
-    isbn = models.CharField(max_length=15, null=False, unique=True,primary_key=True)
-    titulo = models.CharField(max_length=20,null=False)
+class Transacion(models.Model):
+    """
+    Model representing a transaction.
+    """
+    id = models.AutoField(primary_key=True)
+    tipo_transaccion = models.CharField(max_length=15,null=False)
+    fecha_realizacion = models.DateTimeField(auto_now_add=True)
+    cantidad_ejemplares = models.IntegerField(null=False)
+
+    class Meta:
+        verbose_name_plural = "Transaccion"
+class Libro(models.Model):
+    """
+    Model representing a book.
+    """
+    isbn = models.CharField(max_length=15, null=False, unique=True, primary_key=True)
+    titulo = models.CharField(max_length=20, null=False)
     precioCompra = models.IntegerField(null=False)
     precioVenta = models.IntegerField(null=False)
     stock = models.IntegerField(null=False)
+    transaccion = models.ForeignKey(Transacion, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name_plural = "Libro"
+
+    
+
--- backend/libros/views.py
+++ backend/libros/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import *
+from rest_framework import viewsets,permissions
+from .serializers import LibrosSerializers
 
 # Create your views here.
+class LibrosViewSet(viewsets.ModelViewSet):
+    abastecimiento = Libro.objects.
+    queryset = Libro.objects.all()#me consulta todos los objetos de la tabla
+    permission_classes = [permissions.AllowAny]#verifica quien puede acceder a los datos, autenticacion
+    #en allowAny todos pueden acceder
+    serializer_class = LibrosSerializers
+
+
+#class 
