eebae66ec50e949201e60b6cfad18e4bf63c2068
Santiago Angel Mancera <eyerforeversam@gmail.com>
2024-05-31 16:50:32 -0500

    admin add

--- backend/libros/models.py
+++ backend/libros/models.py
@@ -9,7 +9,11 @@ class Libro(models.Model):
     precioCompra = models.IntegerField(null=False)
     precioVenta = models.IntegerField(null=False)
     stock = models.IntegerField(null=False)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    #user = models.ForeignKey(User,on_delete=models.CASCADE)
+    """
+    Se traeran los datos al hacer el select pero todo lo relacionado al libro sera
+    guardado en otra tabla
+    """
     class Meta:
         verbose_name_plural = "Libro"
 
@@ -26,3 +30,10 @@ class Transacion(models.Model):
         verbose_name_plural = "Transaccion"
     
 
+class LibroUser(models.Model):
+    isbn = models.CharField(max_length=15, null=False, unique=True, primary_key=True)
+    titulo = models.CharField(max_length=20, null=False)
+    precioCompra = models.IntegerField(null=False)
+    precioVenta = models.IntegerField(null=False)
+    stock = models.IntegerField(null=False)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
--- backend/libros/urls.py
+++ backend/libros/urls.py
@@ -1,5 +1,8 @@
 from rest_framework import routers #crea las routas automaticamente
 from .views import *
+from django.contrib import admin
+
+
 
 #ejecuto los routers
 router = routers.DefaultRouter()
